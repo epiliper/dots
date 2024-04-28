ac2516de878c36048fb5507b99b706da38a231f6
epiliper <epiliper@outlook.com>
2024-04-28 15:23:24 -0700

    moves repo to entire .config/

---
+++ nvim/init.lua
@@ -0,0 +1,44 @@
+-- LAZY CONFIG
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+-- require("vim-options")
+require("lazy").setup("plugins")
+
+-- keymaps and misc changes 
+require("eli.remap")
+
+vim.keymap.set("n", "<leader>y", "'+y")
+vim.keymap.set("v", "<leader>y", "'+y")
+vim.keymap.set("n", "<leader>Y", "'+Y")
+
+vim.opt.number = true
+vim.opt.rnu = true
+vim.o.termguicolors = true
+
+vim.cmd("hi! Normal guibg=NONE ctermbg=NONE")
+vim.cmd("hi! NonText guibg=NONE ctermbg=NONE")
+
+vim.api.nvim_set_option("clipboard", "unnamed")
+
+-- require("ibl").setup()
+cmdheight = 0
+
+-- telescope configuration
+local builtin = require('telescope.builtin')
+
+vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
+vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
+vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
+
---
+++ nvim/lua/plugins/plugins.lua
@@ -0,0 +1,76 @@
+plugins = {
+
+	{ "ms-jpq/coq_nvim" },
+
+	{ "nvim-lua/plenary.nvim" },
+
+	{ "nvim-tree/nvim-web-devicons",
+		config = function()
+			require('nvim-web-devicons').setup()
+		end,
+	},
+
+	{ "tpope/vim-commentary" },
+
+	{ "nvim-telescope/telescope.nvim" },
+
+	{ "lukas-reineke/indent-blankline.nvim", 
+		main="ibl",
+		ops = {},
+		config = function()
+			require("ibl").setup()
+		end,
+
+	},
+
+	{ "lukas-reineke/lsp-format.nvim" },
+
+	{ "rcarriga/nvim-dap-ui",
+	dependencies = {
+			'nvim-neotest/nvim-nio'
+		},
+	},
+
+ 	{
+ 		"neanias/everforest-nvim",
+ 		priority = 1000,
+ 		lazy = false,
+ 		version = false,
+ 		config = function()
+ 			vim.cmd([[colorscheme everforest]])
+ 			require("everforest").setup({
+ 				background = "medium",
+ 				transparent_background_level = 1,
+			})
+		end,
+	 },
+
+	{
+		"lukas-reineke/headlines.nvim",
+		dependencies = "nvim-treesitter/nvim-treesitter",
+		config = true,
+		},
+	{
+		'nvim-lualine/lualine.nvim',
+		dependencies = { 'nvim-tree/nvim-web-devicons'},
+		config = function()
+			require('lualine').setup()
+		end,
+		},
+	{
+		"iamcco/markdown-preview.nvim",
+		cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop"},
+		ft = { "markdown"},
+		build = function() vim.fn["mkdp#util#install"]() end,
+
+		},
+	{
+		"windwp/nvim-autopairs",
+		event = "InsertEnter",
+		config = true,
+		-- use opts = {} for passing setup options
+		-- this is equalent to setup({}) function
+	},
+}
+
+return plugins
